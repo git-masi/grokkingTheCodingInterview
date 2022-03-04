def28974391c1d75899fd91e926318ebe782393e
git-masi <eric.masi.dev@gmail.com>
2022-03-04 11:48:20 -0500

    Attempt cycle in circular array

---
+++ fast_slow_pointers/cycle-in-circular-array.js
@@ -0,0 +1,49 @@
+console.log(hasCycle([1, 2, -1, 2, 2]));
+
+console.log(hasCycle([2, 2, -1, 2]));
+
+console.log(hasCycle([2, 1, -1, -2]));
+
+console.log(hasCycle([1]));
+
+console.log(hasCycle([1, -1]));
+
+function hasCycle(arr) {
+  if (arr.length < 2) return true;
+
+  let fast = 0;
+  let slow = 0;
+
+  do {
+    fast = move(arr[fast], fast);
+    fast = move(arr[fast], fast);
+    slow = move(arr[slow], slow);
+  } while (fast !== slow);
+
+  let anchor = fast;
+  let isPos = arr[anchor] > 0;
+
+  do {
+    slow = move(arr[slow], slow);
+
+    if (arr[slow] > 0 !== isPos) {
+      return false;
+    }
+  } while (slow !== anchor);
+
+  return true;
+
+  function move(steps, pointer) {
+    while (steps !== 0) {
+      if (steps < 0) {
+        pointer = pointer === 0 ? arr.length - 1 : pointer - 1;
+        steps++;
+      } else if (steps > 0) {
+        pointer = pointer === arr.length - 1 ? 0 : pointer + 1;
+        steps--;
+      }
+    }
+
+    return pointer;
+  }
+}
