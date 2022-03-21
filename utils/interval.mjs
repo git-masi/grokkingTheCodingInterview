6fb52b69854c8866043b0bc8e5fee3933a2b1918
git-masi <eric.masi.dev@gmail.com>
2022-03-21 09:04:16 -0400

    DRY code by adding reusable fn to map arrays to Interval objects

--- utils/interval.mjs
+++ utils/interval.mjs
@@ -12,3 +12,14 @@ export class Interval {
     console.log(`${this.toArray()}`);
   }
 }
+
+/**
+ * Takes in a 2d array of ints representing start and end intervals and returns
+ * an arry of interval objects
+ *
+ * @param {[number, number][]} arr [[start, end], [start, end]]
+ * @returns {Record<'start': number, 'end': number>[]} Array of Interval objects
+ */
+export function mapToIntervals(arr) {
+  return arr.map(([start, end]) => new Interval(start, end));
+}
